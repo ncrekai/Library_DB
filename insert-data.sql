a769772c110324ff79409ca66e773a4c65f4a7a6
ncrekai <nrekai@my.centennialcollege.ca>
2024-11-12 14:50:48 -0500

    All inserts but HOLDS

--- insert-data.sql
+++ insert-data.sql
@@ -147,14 +147,123 @@ INSERT INTO collection VALUES(
 
 -- Add data / PATRONS
 
+INSERT INTO patrons VALUES(
+   patron_id_seq.NEXTVAL, 121053, 'Bob', 'Bumble', '12-May-50', '123 Sesame St.', '416-457-6527', 'bbumble@somewhere.com', 2, 'b_bmbl', '123abc'
+);
+INSERT INTO patrons VALUES(
+   patron_id_seq.NEXTVAL, 121323, 'Cassandra', 'Cory', '04-Jun-56', '124 Sesame St', '416-773-7914', 'ccory@somewhere.com', 5, 'c_cry', '234bcd'
+);
+INSERT INTO patrons VALUES(
+   patron_id_seq.NEXTVAL, 121330, 'Davendra', 'Davis', '14-Sep-67', '125 Sesame St.', '647-586-6546', 'ddavis@somewhere.com', 3, 'd_dvs', '345cde'
+);
+INSERT INTO patrons VALUES(
+   patron_id_seq.NEXTVAL, 121409, 'Eric', 'Ellroy', '27-Dec-84', '126 Sesame St.', '416-757-2864', 'eellroy@somewhere.com', 1, 'e_ellry', '456def'
+);
+INSERT INTO patrons VALUES(
+   patron_id_seq.NEXTVAL, 121591, 'Femi', 'Ferris', '08-Jul-96', '127 Sesame St.', '905-535-8529', 'fferris@somewhere.com', 1, 'f_frrs', '567efg'
+);
+INSERT INTO patrons VALUES(
+   patron_id_seq.NEXTVAL, 121647, 'Gary', 'Gimble', '18-Mar-04', '128 Sesame St.', '647-425-7932', 'ggimble@somewhere.com', 6, 'g_gmbl', '678fgh'
+);
+
 -- Add data / CHECKOUTS
+
+INSERT INTO checkouts (
+   id, patron_id, card_number, collection_id, branch_id, checkout_date, due_date
+) VALUES (
+   1, 3, 121330, 3, 5, '01-Nov-24', '15-Nov-24'
+);
+INSERT INTO checkouts (
+   id, patron_id, card_number, collection_id, branch_id, checkout_date, due_date
+) VALUES (
+   2, 3, 121330, 5, 5, '01-Nov-24', '15-Nov-24'
+);
+INSERT INTO checkouts (
+   id, patron_id, card_number, collection_id, branch_id, checkout_date, due_date
+) VALUES (
+   4, 4, 121409, 11, 4, '22-Oct-24', '05-Nov-24'
+);
+INSERT INTO checkouts VALUES (
+   5, 1, 121053, 13, 3, '17-Oct-24', '31-Oct-24', '23-Oct-24'
+);
+INSERT INTO checkouts VALUES (
+   6, 1, 121053, 9, 3, '17-Oct-24', '31-Oct-24', '20-Oct-24'
+);
 
 -- Add data / BRANCH
 
+INSERT INTO branch VALUES(
+   1, 'Rexdale', '2243 Kipling Avenue Toronto, ON  M9W 4L5', '416-394-5200'
+);
+INSERT INTO branch VALUES(
+   2, 'Parkdale', '1303 Queen Street West Toronto, ON  M6K 1L6', '416-393-7686'
+);
+INSERT INTO branch VALUES(
+   3, 'Gerrard/Ashdale', '1432 Gerrard Street East Toronto, ON  M4L 1Z6', '416-393-7717'
+);
+INSERT INTO branch VALUES(
+   4, 'High Park', '228 Roncesvalles Avenue Toronto, ON  M6R 2L7', '416-393-7671'
+);
+INSERT INTO branch VALUES(
+   5, 'Leaside', '165 McRae Drive Toronto, ON  M4G 1S8', '416-396-3835'
+);
+INSERT INTO branch VALUES(
+   6, 'Mount Pleasant', '599 Mt. Pleasant Road Toronto, ON  M4S 2M5', '416-393-7737'
+);
+
 -- Add data / BOOK_STATUS
+
+INSERT INTO book_status VALUES (1, 'In library');
+INSERT INTO book_status VALUES (2, 'Checked out');
+INSERT INTO book_status VALUES (3, 'On hold');
+INSERT INTO book_status VALUES (4, 'In transit');
+INSERT INTO book_status VALUES (5, 'Lost');
+INSERT INTO book_status VALUES (6, 'Removed');
 
 -- Add data / GENRE
 
+INSERT INTO genre VALUES (1, 'General Fiction');
+INSERT INTO genre VALUES (2, 'Historical Fiction');
+INSERT INTO genre VALUES (3, 'Horror');
+INSERT INTO genre VALUES (4, 'Fantasy');
+INSERT INTO genre VALUES (5, 'Mysteries / Suspense  / Thrillers');
+INSERT INTO genre VALUES (6, 'Romance');
+INSERT INTO genre VALUES (7, 'Science Fiction');
+INSERT INTO genre VALUES (8, 'Graphic Novels');
+INSERT INTO genre VALUES (9, 'Arts and Photography');
+INSERT INTO genre VALUES (10, 'Biography');
+INSERT INTO genre VALUES (11, 'Business and Finance');
+INSERT INTO genre VALUES (12, 'Careers and Job Searching');
+INSERT INTO genre VALUES (13, 'Computers and Technology');
+INSERT INTO genre VALUES (14, 'Crafts, Hobbies and Home');
+INSERT INTO genre VALUES (15, 'Food and Drink');
+INSERT INTO genre VALUES (16, 'French');
+INSERT INTO genre VALUES (17, 'Health and Wellness');
+INSERT INTO genre VALUES (18, 'History');
+INSERT INTO genre VALUES (19, 'Music and Performing Arts');
+INSERT INTO genre VALUES (20, 'Parenting and Relationships');
+INSERT INTO genre VALUES (21, 'Philosophy and Religion');
+INSERT INTO genre VALUES (22, 'Politics, Government and Law');
+INSERT INTO genre VALUES (23, 'Science and the Environment');
+INSERT INTO genre VALUES (24, 'Social Issues');
+INSERT INTO genre VALUES (25, 'Sports and Games');
+INSERT INTO genre VALUES (26, 'Travel');
+
 -- Add data / LANGUAGE
+
+INSERT INTO language VALUES (1, 'English');
+INSERT INTO language VALUES (2, 'French');
+INSERT INTO language VALUES (3, 'Chinese');
+INSERT INTO language VALUES (4, 'Tamil');
+INSERT INTO language VALUES (5, 'Hindi');
+INSERT INTO language VALUES (6, 'Russian');
+INSERT INTO language VALUES (7, 'Polish');
+INSERT INTO language VALUES (8, 'German');
+INSERT INTO language VALUES (9, 'Korean');
+INSERT INTO language VALUES (10, 'Greek');
+INSERT INTO language VALUES (11, 'Tagalog');
+INSERT INTO language VALUES (12, 'Spanish');
+INSERT INTO language VALUES (13, 'Urdu');
+INSERT INTO language VALUES (14, 'Somali');
 
 -- Add data / HOLDS
