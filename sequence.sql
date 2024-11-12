b49ed492037b7868c8592d2cf877b4dcf2fffa51
ncrekai <nrekai@my.centennialcollege.ca>
2024-11-12 14:54:29 -0500

    CREATE seq x3 / UPDATE with seq x1

--- sequence.sql
+++ sequence.sql
@@ -1,3 +1,23 @@
 -- CREATE sequence
 
--- UPDATE table with sequence
+CREATE SEQUENCE book_id_seq 
+    MINVALUE 1
+    MAXVALUE 999999
+    INCREMENT BY 1;
+
+CREATE SEQUENCE patron_id_seq 
+    MINVALUE 1
+    MAXVALUE 999999
+    INCREMENT BY 1;
+
+CREATE SEQUENCE collection_id_seq 
+    MINVALUE 1
+    MAXVALUE 999999
+    INCREMENT BY 1;
+
+-- UPDATE COLLECTION -> ID with sequence
+
+BEGIN
+    UPDATE COLLECTION
+    SET id = collection_id_seq.nextval;
+END;
