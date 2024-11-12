1b79f632612cae73d5c0b3031c6e0f3d40c6dc05
ncrekai <nrekai@my.centennialcollege.ca>
2024-11-12 08:46:14 -0500

    insert data file

---
+++ insert-data.sql
@@ -0,0 +1,20 @@
+-- Create tables
+
+
+-- Add data / BOOKS
+
+-- Add data / COLLECTION
+
+-- Add data / PATRONS
+
+-- Add data / CHECKOUTS
+
+-- Add data / BRANCH
+
+-- Add data / BOOK_STATUS
+
+-- Add data / GENRE
+
+-- Add data / LANGUAGE
+
+-- Add data / HOLDS
