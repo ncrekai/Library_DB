3f1d0af58525547eee1b094d5eb35f2699d91312
mudelinelsisy <mudelinelsisy@yahoo.com>
2024-11-17 02:27:50 -0500

    Update indexes.sql

--- indexes.sql
+++ indexes.sql
@@ -1,1 +1,20 @@
---CREATE indexes
+CREATE INDEX patron_name_idx
+    ON patrons(fname, lname);
+
+CREATE INDEX book_title_idx
+    ON books(title);
+
+CREATE INDEX author_idx
+    ON books(author_fname, author_lname);
+
+CREATE UNIQUE INDEX branch_name_idx
+    ON branch(branch_name);
+
+CREATE INDEX genre_name_idx
+    ON genre(genre_name);
+
+CREATE UNIQUE INDEX email_idx
+    ON patrons(email);
+
+CREATE UNIQUE INDEX phone_idx
+    ON patrons(patron_phone);
